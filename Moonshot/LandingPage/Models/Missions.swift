6c15a13b30179e72cc832f9d66aab11ef5b8e398
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-08-19 21:18:47 -0700

    Implemented missions model

--- Moonshot/LandingPage/Models/Missions.swift
+++ Moonshot/LandingPage/Models/Missions.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Missions: Codable {
+    var missions: [Mission]
+}
+
+struct Mission: Codable, Identifiable {
+    var id: Int?
+    var launchDate: Date?
+    var crew: [CrewMember]?
+    var description: String?
+}
+
+struct CrewMember: Codable {
+    var name: String?
+    var role: String?
+}
