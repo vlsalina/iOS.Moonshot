73aade450b333f4dd75fc0c02f48816799d35b4b
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-08-19 20:53:08 -0700

    First commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Vincent Salinas on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
