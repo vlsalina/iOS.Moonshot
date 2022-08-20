00aea283fb08cbb15dd7185b9ecb0c0c91916b28
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-08-19 21:01:27 -0700

    Implemented astronaut model

---
+++ Moonshot/LandingPage/Models/Astronauts.swift
@@ -0,0 +1,15 @@
+//
+//  Astronauts.swift
+//  Moonshot
+//
+//  Created by Vincent Salinas on 8/19/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    var id: String?
+    var name: String?
+    var description: String?
+}
+
